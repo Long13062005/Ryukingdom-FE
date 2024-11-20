a4cdf671337dd08da604af6dbcb4c5cdb19a88a5
Long13062005 <vulong13062005@gmail.com>
2024-11-20 17:07:16 +0700

    test

--- src/components/layout/ErrorPage.js
+++ src/components/layout/ErrorPage.js
@@ -1,18 +1,9 @@
-import {useCallback, useEffect, useState} from "react";
-import Swal from "sweetalert2";
-import {useNavigate} from "react-router-dom";
-import {toast} from "react-toastify";
-import ContractService from "../../services/ContractService";
-import AuthService from "../../services/AuthService";
-
 export function ErrorPage() {
 
 
-    return (
-        <div>
+    return (<div>
             <div className="row justify-content-center" style={{margin: "30px 0 30px 0"}}>
-                <h1 style={{color:"red"}}>404 NOT FOUND</h1>
+                <h1 style={{color: "red"}}>404 NOT FOUND</h1>
             </div>
-        </div>
-    );
+        </div>);
 }
--- src/components/layout/SideBar.js
+++ src/components/layout/SideBar.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {Link, NavLink} from "react-router-dom";
-import "bootstrap/js/src/theme";
 import AuthService from "../../services/AuthService";
 
 
